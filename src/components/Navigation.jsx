65c2b02af5986f300b988472742204df4f125c1b
Hans Nilsson <hn222yi@student.lnu.se>
2026-04-21 11:47:40 +0200

    Add initial components

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+function Navigation() {
+  return (
+    <nav>
+      <h1>Flick</h1>
+    </nav>
+  )
+}
+
+export default Navigation
---
+++ src/pages/DiscoveryPage.jsx
@@ -0,0 +1,9 @@
+import CardPlaceholder from '../assets/card-placeholder.png'
+
+function DiscoveryPage() {
+  return (
+    <img src={CardPlaceholder} alt="Card placeholder" />
+  )
+}
+
+export default DiscoveryPage
