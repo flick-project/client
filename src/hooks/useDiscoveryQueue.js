6b1b32bcc227bd81c2eb64e695a30be9f872eb7a
Lingonkvist <hans@lingon.cloud>
2026-07-08 11:03:49 +0200

    chore: update jsdoc

--- src/hooks/useDiscoveryQueue.js
+++ src/hooks/useDiscoveryQueue.js
@@ -2,7 +2,8 @@ import { use } from 'react'
 import { DiscoveryContext } from '../context/DiscoveryContext.jsx'
 
 /**
- *
+ * Hook to access discovery queue functionality.
+ * @returns {object} Queue state and functions.
  */
 export function useDiscoveryQueue () {
   return use(DiscoveryContext)
